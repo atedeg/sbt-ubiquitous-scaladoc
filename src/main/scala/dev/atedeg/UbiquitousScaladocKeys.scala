2dabce45340fe3293b10bb00e8d24385b0fcaa77
Linda Vitali <linda.vitali2@studio.unibo.it>
2022-06-04 13:35:14 +0200

    refactor: plugin keys description

--- src/main/scala/dev/atedeg/UbiquitousScaladocKeys.scala
+++ src/main/scala/dev/atedeg/UbiquitousScaladocKeys.scala
@@ -3,10 +3,13 @@ package dev.atedeg
 import sbt.*
 
 trait UbiquitousScaladocKeys {
-  val ubiquitousScaladoc: TaskKey[Unit] = taskKey[Unit]("Creating an Ubiquitous Language markdown table out of a Scaladoc html file")
 
-  val usSourceHtmlDir: SettingKey[File] = settingKey[File]("Source directory to generate ubiquitous language markdown table from")
+  val ubiquitousScaladoc: TaskKey[Unit] =
+    taskKey[Unit]("Task for creating a Ubiquitous Language markdown table out of a Scaladoc html file")
 
-  val usTargetMarkdownDir: SettingKey[File] = settingKey[File]("Target directory to store generated ubiquitous language markdown table")
+  val usSourceHtmlDir: SettingKey[File] =
+    settingKey[File]("The source directory to generate ubiquitous language markdown tables from")
 
+  val usTargetMarkdownDir: SettingKey[File] =
+    settingKey[File]("The target directory where to put the generated ubiquitous language markdown tables")
 }
