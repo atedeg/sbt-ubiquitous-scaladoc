33aee6207d52420b32cc7b121aa54ac7ceacfc3d
Linda Vitali <linda.vitali2@studio.unibo.it>
2022-06-19 15:54:46 +0200

    fix: get branch name from git command

--- release.sh
+++ release.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-branch=$1
-nextReleaseVersion=$2
+branch=$(git rev-parse --abbrev-ref HEAD)
+nextReleaseVersion=$1
+
+echo "Current branch: $branch"
+
 if [[ $branch == "main" ]]; then
     sbt +publishSigned || exit 1
     sbt sonatypeBundleRelease || exit 2
     echo "Published on Maven Central"
 fi
-#git tag -a -f "$nextReleaseVersion" "$nextReleaseVersion" -F CHANGELOG.md
-#git push --force origin "$nextReleaseVersion"
-echo "Pushed annotated tag: $nextReleaseVersion"
