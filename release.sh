01fa2acaef6001c1a95ad9665ffe8a6438a9447e
ndido98 <ndido98@gmail.com>
2022-06-19 11:18:39 +0200

    ci: add ci

---
+++ release.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+branch=$1
+nextReleaseVersion=$2
+if [[ $branch == "main" ]]; then
+    sbt +publishSigned || exit 1
+    sbt sonatypeBundleRelease || exit 2
+    echo "Published on Maven Central"
+fi
+git tag -a -f "$nextReleaseVersion" "$nextReleaseVersion" -F CHANGELOG.md
+git push --force origin "$nextReleaseVersion"
+echo "Pushed annotated tag: $nextReleaseVersion"
