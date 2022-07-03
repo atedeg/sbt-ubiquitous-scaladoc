6092729abf2e8c8af7bac76c676d601d385e6083
Linda Vitali <linda.vitali2@studio.unibo.it>
2022-07-03 20:34:21 +0200

    feat: add multiple table reading and ignored files

--- src/main/scala/dev/atedeg/Configuration.scala
+++ src/main/scala/dev/atedeg/Configuration.scala
@@ -1,15 +1,36 @@
 package dev.atedeg
 
+import scala.util.Try
+
+import better.files.File
 import io.circe.Json
-import io.circe.generic.auto._
+import io.circe.generic.auto.*
 import io.circe.yaml.parser
+import cats.implicits._
 
 final case class Selector(selector: String)
 final case class Configuration(ignored: List[Selector], tables: List[TableConfig])
 final case class TableConfig(name: String, columns: List[ColumnConfig], rows: List[Selector])
 final case class ColumnConfig(name: String, selector: String)
 
-object ParseConfiguration {
-  def parseConfiguration(raw: String): Option[Configuration] = parser.parse(raw).toOption.flatMap(parseJson)
+object Configuration {
+
+  def read(defaultLocation: File): Either[String, Configuration] =
+    Try((defaultLocation / ".unidoc").contentAsString).toEither.leftMap(_.toString).flatMap(parse)
+
+  private def parse(raw: String): Either[String, Configuration] =
+    parser.parse(raw).toOption.flatMap(parseJson).toRight("Can not parse configuration file")
   private def parseJson(json: Json): Option[Configuration] = json.as[Configuration].toOption
+}
+
+object Selector {
+
+  def toFiles(workingDir: File, selector: Selector): Either[String, List[File]] = {
+    val file = File(workingDir, selector.selector)
+    Try(
+      if (file.isDirectory) file.listRecursively.toList.sortBy(_.name)
+      else if (file.exists) List(file)
+      else workingDir.glob(selector.selector).toList.sortBy(_.name),
+    ).toEither.leftMap(_.toString)
+  }
 }
