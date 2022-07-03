13db85ebad15a45d18a651208b2b11bef4154d58
Linda Vitali <linda.vitali2@studio.unibo.it>
2022-07-03 18:34:38 +0200

    feat!: plugin configuration with yml file

--- src/main/scala/dev/atedeg/UbiquitousScaladocKeys.scala
+++ src/main/scala/dev/atedeg/UbiquitousScaladocKeys.scala
@@ -7,18 +7,9 @@ trait UbiquitousScaladocKeys {
   val ubidoc: TaskKey[Unit] =
     taskKey[Unit]("Task for creating a Ubiquitous Language markdown table out of a Scaladoc html file")
 
-  val htmlTags: SettingKey[Seq[String]] =
-    settingKey[Seq[String]]("The tags to be extracted from the html file")
+  val workingDirectory: SettingKey[File] =
+    settingKey[File]("The base directory where to look for the html files")
 
-  val tableHeaders: SettingKey[Seq[String]] =
-    settingKey[Seq[String]]("The headers of the markdown table")
-
-  val fileNameSuffix: SettingKey[String] =
-    settingKey[String]("The suffix of the generated file's name")
-
-  val sourceHtmlDir: SettingKey[File] =
-    settingKey[File]("The source directory to generate ubiquitous language markdown tables from")
-
-  val targetMarkdownDir: SettingKey[File] =
-    settingKey[File]("The target directory where to put the generated ubiquitous language markdown tables")
+  val targetDirectory: SettingKey[File] =
+    settingKey[File]("The target directory where to put the generated Markdown tables")
 }
