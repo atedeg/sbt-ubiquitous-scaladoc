54ffdfb2b6bf0e1e09c15e4b301a677134a9b109
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2022-07-04 13:17:26 +0200

    feat: change configuration spec to avoid connflicts

--- src/main/scala/dev/atedeg/Configuration.scala
+++ src/main/scala/dev/atedeg/Configuration.scala
@@ -1,40 +1,36 @@
 package dev.atedeg
 
 import scala.util.Try
-
 import better.files.File
-import io.circe.{ Decoder, Json }
-import io.circe.generic.auto._
+import io.circe.{Decoder, Json}
+import io.circe.generic.auto.*
 import io.circe.yaml.parser
-import cats.implicits._
+import cats.implicits.*
+import Extensions.*
 
-import Extensions._
-
-final case class Selector(selector: String) {
-
-  def toFiles(workingDir: File): Either[String, List[File]] = {
-    Try(unsafeToFiles(workingDir))
+sealed trait Selector {
+  def toFiles(workingDir: File): Either[String, List[File]] =
+    Try(unsafeToFiles(workingDir, this))
       .map(_.sortBy(_.name))
       .toEither
-      .filterOrElse(_.nonEmpty, s"No files found matching selector $selector")
+      .filterOrElse(_.nonEmpty, s"No files found matching selector $this")
+
+  private def unsafeToFiles(workingDir: File, s: Selector): List[File] = s match {
+    case FileSelector(file) => workingDir.listHtmlFiles.find(_.nameWithoutExtension == file).toList
+    case DirSelector(dir) => htmlFilesInDirectory(File(workingDir, dir))
+    case GlobSelector(glob) => workingDir.globHtmlFiles(glob).toList
   }
-
-  private def unsafeToFiles(workingDir: File): List[File] =
-    htmlFilesInDirectory(File(workingDir, selector)) ++ selectorToFileOrGlob(workingDir)
-
-  private def selectorToFileOrGlob(workingDir: File): List[File] =
-    workingDir.listHtmlFiles.find(_.nameWithoutExtension == selector) match {
-      case Some(f) => List(f)
-      case None => workingDir.globHtmlFiles(selector).toList
-    }
 
   private def htmlFilesInDirectory(dir: File): List[File] =
     if (dir.isDirectory) dir.listHtmlFiles.toList else List()
 }
+final case class FileSelector(file: String) extends Selector
+final case class DirSelector(dir: String) extends Selector
+final case class GlobSelector(glob: String) extends Selector
 
 final case class Configuration(ignored: List[Selector], tables: List[TableConfig])
 final case class TableConfig(name: String, columns: List[ColumnConfig], rows: List[Selector])
-final case class ColumnConfig(name: String, selector: String)
+final case class ColumnConfig(name: String, htmlTag: String)
 
 object Configuration {
   private val configFile = ".ubidoc"
@@ -44,8 +40,15 @@ object Configuration {
 
   def parse(raw: String): Either[String, Configuration] = parser.parse(raw).flatMap(parseJson)
 
+  @SuppressWarnings(Array("org.wartremover.warts.IterableOps"))
   private def parseJson(json: Json): Either[String, Configuration] = {
-    implicit val decoder: Decoder[List[Selector]] = Decoder.decodeList(Decoder.decodeString.map(Selector))
+    implicit val decodeSelector: Decoder[Selector] = {
+      List[Decoder[Selector]](
+        Decoder[FileSelector].widen,
+        Decoder[DirSelector].widen,
+        Decoder[GlobSelector].widen,
+      ).reduceLeft(_ or _)
+    }
     json.as[Configuration]
   }
 }
