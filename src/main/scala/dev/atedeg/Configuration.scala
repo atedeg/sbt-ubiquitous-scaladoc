e85e1eff1abc9b3a551c256aa94511833c61d43d
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2022-07-04 11:06:38 +0200

    refactor: improve error message reporting

--- src/main/scala/dev/atedeg/Configuration.scala
+++ src/main/scala/dev/atedeg/Configuration.scala
@@ -7,7 +7,17 @@ import io.circe.generic.auto.*
 import io.circe.yaml.parser
 import cats.implicits.*
 
-final case class Selector(selector: String)
+final case class Selector(selector: String) {
+  def toFiles(workingDir: File): Either[String, List[File]] = {
+    val file = File(workingDir, selector)
+    Try(
+      if (file.isDirectory) file.listRecursively.toList.sortBy(_.name)
+      else if (file.exists) List(file)
+      else workingDir.glob(selector).toList.sortBy(_.name),
+    ).toEither.leftMap(_.toString)
+  }
+}
+
 final case class Configuration(ignored: List[Selector], tables: List[TableConfig])
 final case class TableConfig(name: String, columns: List[ColumnConfig], rows: List[Selector])
 final case class ColumnConfig(name: String, selector: String)
@@ -18,24 +28,10 @@ object Configuration {
   def read(defaultLocation: File): Either[String, Configuration] =
     Try((defaultLocation / configFile).contentAsString).toEither.leftMap(_.toString).flatMap(parse)
 
-  def parse(raw: String): Either[String, Configuration] =
-    parser.parse(raw).toOption.flatMap(parseJson).toRight("Can not parse configuration file")
+  def parse(raw: String): Either[String, Configuration] = parser.parse(raw).leftMap(_.toString).flatMap(parseJson)
 
-  private def parseJson(json: Json): Option[Configuration] = {
-    implicit val decoder: Decoder[List[Selector]] =
-      Decoder.decodeList(Decoder.decodeString.map(Selector(_)))
-    json.as[Configuration].toOption
-  }
-}
-
-object Selector {
-
-  def toFiles(workingDir: File, selector: Selector): Either[String, List[File]] = {
-    val file = File(workingDir, selector.selector)
-    Try(
-      if (file.isDirectory) file.listRecursively.toList.sortBy(_.name)
-      else if (file.exists) List(file)
-      else workingDir.glob(selector.selector).toList.sortBy(_.name),
-    ).toEither.leftMap(_.toString)
+  private def parseJson(json: Json): Either[String, Configuration] = {
+    implicit val decoder: Decoder[List[Selector]] = Decoder.decodeList(Decoder.decodeString.map(Selector))
+    json.as[Configuration].leftMap(_.toString)
   }
 }
