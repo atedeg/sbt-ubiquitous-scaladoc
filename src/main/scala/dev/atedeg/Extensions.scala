2d6ce55736b1ab24d1da340c6e94843b64e8e8bb
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2022-07-15 13:56:48 +0200

    fix: fix error message typing

--- src/main/scala/dev/atedeg/Extensions.scala
+++ src/main/scala/dev/atedeg/Extensions.scala
@@ -1,20 +1,11 @@
 package dev.atedeg
 
-import scala.language.implicitConversions
-
 import better.files.File
-import cats.implicits.*
 
 object Extensions {
-  implicit def eitherToEitherString[A, E](e: Either[E, A]): Either[String, A] = e.leftMap(_.toString)
-
-  implicit class BetterFileIterator(val i: Iterator[File]) {
-    def keepHtmlFiles: Iterator[File] = i.filter(_.isHtmlFile)
-  }
 
   implicit class BetterFile(val f: File) {
     def isHtmlFile: Boolean = f.extension.contains(".html")
-    def listHtmlFiles: Iterator[File] = f.listRecursively.keepHtmlFiles
-    def globHtmlFiles(g: String): Iterator[File] = f.glob(g).keepHtmlFiles
+    def listHtmlFiles: Iterator[File] = f.listRecursively.filter(_.isHtmlFile)
   }
 }
