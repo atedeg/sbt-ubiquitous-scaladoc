77aa9e954373f841e9da11a10b0a7bf17da165e0
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2022-07-15 13:29:54 +0200

    feat: use new selector kind

--- src/main/scala/dev/atedeg/HtmlParsing.scala
+++ src/main/scala/dev/atedeg/HtmlParsing.scala
@@ -1,34 +1,51 @@
 package dev.atedeg
 
-import net.ruippeixotog.scalascraper.browser.Browser
+import better.files.File
+import net.ruippeixotog.scalascraper.browser.{ Browser, JsoupBrowser }
 import net.ruippeixotog.scalascraper.scraper.ContentExtractors.*
 import net.ruippeixotog.scalascraper.model.*
 import net.ruippeixotog.scalascraper.dsl.DSL.*
+import cats.syntax.all.*
 
 object HtmlParsing {
 
-  def extractColumn(
-      document: Browser#DocumentType,
-      fileName: String,
-      columnConfig: ColumnConfig,
-  ): Either[String, String] = {
-    (document >?> element(columnConfig.htmlTag))
-      .map(_.childNodes)
-      .map(toMarkdown(_, fileName))
-      .toRight(s"Cannot extract column ${columnConfig.name}")
-  }
+  def extractClassLike(file: File): Either[Error, (String, String)] = for {
+    document <- JsoupBrowser().parseFile(file.toJava).asRight
+    term <- extractTagFromDocument(file, document, "title")
+    definition <- extractTagFromDocument(file, document, "div.doc > p")
+  } yield (term, definition)
 
-  private def toMarkdown(es: Iterable[Node], fileName: String): String = {
+  private def extractTagFromElem(file: File, elem: Element, tag: String): Either[Error, String] =
+    elem.tryExtract(element(tag)).map(_.childNodes).map(toMarkdown).toRight(ParseError(file, tag))
+
+  private def extractTagFromDocument(file: File, doc: Browser#DocumentType, tag: String): Either[Error, String] =
+    doc.tryExtract(element(tag)).map(_.childNodes).map(toMarkdown).toRight(ParseError(file, tag))
+
+  private def toMarkdown(es: Iterable[Node]): String = {
+    def isLink(e: Element): Boolean = e.tagName == "a"
+    def toMarkdownLink(e: Element): String = s"[${extractName(e.text)}](${e.text})"
+    def extractName(fullPath: String): String = fullPath.split('.').last
+
     es.foldLeft("") { (acc, elem) =>
       elem match {
         case TextNode(s) => acc + s
-        case ElementNode(e) if isLink(e) => acc + toMarkdownLink(e, fileName)
-        case ElementNode(e) => acc + toMarkdown(e.childNodes, fileName)
+        case ElementNode(e) if isLink(e) => acc + toMarkdownLink(e)
+        case ElementNode(e) => acc + toMarkdown(e.childNodes)
       }
     }
   }
 
-  private def isLink(e: Element): Boolean = e.tagName == "a"
-  private def toMarkdownLink(e: Element, fileName: String): String = s"[$fileName](${e.text})"
+  private def extractMany(file: File, doc: Browser#DocumentType, tag: String): Either[Error, List[Element]] =
+    doc.tryExtract(elementList(tag)).toRight(ParseError(file, tag))
 
+  def extractNonClassLike(file: File, name: String): Either[Error, (String, String)] = for {
+    document <- JsoupBrowser().parseFile(file.toJava).asRight
+    elements <- extractMany(file, document, "div.documentableElement")
+    definition <- elements
+      .find(hasName(_, name))
+      .toRight(ParseError(file, "a.documentableName"))
+      .flatMap(extractTagFromElem(file, _, "div.cover > div.doc"))
+  } yield (name, definition)
+
+  private def hasName(elem: Element, name: String): Boolean = elem.tryExtract(text("a.documentableName")).contains(name)
 }
--- src/main/scala/dev/atedeg/Ubidoc.scala
+++ src/main/scala/dev/atedeg/Ubidoc.scala
@@ -2,53 +2,44 @@ package dev.atedeg
 
 import java.io.{ File => JFile }
 
-import scala.util.Try
-
 import better.files.{ File, FileExtensions }
 import cats.implicits.*
 
-import Extensions._
-
 object Ubidoc {
 
-  def apply(workingDir: JFile, targetDir: JFile, baseDir: JFile): Unit =
-    Internals.ubiquitousScaladocTask(workingDir.toScala, targetDir.toScala, baseDir.toScala)
+  def apply(lookupDir: JFile, targetDir: JFile, workingDir: JFile): Unit =
+    Internals.ubiquitousScaladocTask(lookupDir.toScala, targetDir.toScala, workingDir.toScala)
 
   private object Internals {
 
-    def ubiquitousScaladocTask(workingDir: File, targetDir: File, baseDir: File): Unit = {
-      val leftoverFiles = for {
-        conf <- Configuration.read(baseDir)
-        allFiles <- listAllFiles(workingDir)
-        ignoredFiles <- getIgnoredFiles(conf, workingDir)
-        parsedFiles <- parseAllFiles(conf, workingDir, targetDir, ignoredFiles)
-      } yield allFiles -- ignoredFiles -- parsedFiles
-
-      leftoverFiles match {
-        case Left(err) => throw new IllegalStateException(err)
-        case Right(s) if s.isEmpty => println("Done!")
-        case Right(s) => throw new IllegalStateException(s"Unparsed files: $s")
+    def ubiquitousScaladocTask(lookupDir: File, targetDir: File, workingDir: File): Unit = {
+      val result = for {
+        config <- Configuration.read(workingDir)
+        allEntities <- Configuration.readAllEntities
+        consideredEntities = config.tables.flatMap(_.rows).toSet
+        _ <- checkConsistency(allEntities, consideredEntities, config.ignored)
+        tables <- config.tables.traverse(Table.parse(_, lookupDir))
+        _ = tables.foreach(_.serialize(targetDir))
+      } yield ()
+      result match {
+        case Left(err) => throw ???
+        case Right(_) => ()
       }
     }
 
-    def listAllFiles(workingDir: File): Either[String, Set[File]] =
-      Try(workingDir.listHtmlFiles).toEither.map(_.toSet)
-
-    def getIgnoredFiles(conf: Configuration, workingDir: File): Either[String, Set[File]] =
-      conf.ignored.flatTraverse(_.toFiles(workingDir)).map(_.toSet)
-
-    def parseAllFiles(
-        conf: Configuration,
-        workingDir: File,
-        targetDir: File,
-        ignoredFiles: Set[File],
-    ): Either[String, Set[File]] =
-      for {
-        res <- conf.tables.traverse(Table.parse(workingDir, _, ignoredFiles))
-        tables = res.map(_._1)
-        _ <- tables.traverse(_.serialize(targetDir)).toEither
-        files = res.flatMap(_._2)
-      } yield files.toSet
+    private def checkConsistency(
+        allEntities: Set[IgnoredSelector],
+        considered: Set[Selector],
+        ignored: Set[IgnoredSelector],
+    ): Either[Error, Unit] = {
+      val c = considered.map(_.toIgnored)
+      val consideredAndIgnoredIntersection = c.intersect(ignored)
+      val leftoverEntities = allEntities.diff(c).diff(ignored)
+      if (consideredAndIgnoredIntersection.nonEmpty)
+        OverlappingIgnoredAndConsidered(consideredAndIgnoredIntersection).asLeft
+      else if (leftoverEntities.nonEmpty) LeftoverEntities(leftoverEntities).asLeft
+      else ().asRight
+    }
 
   }
 }
