5113a6bc04ec0d3a36d0bf82d20fadbbff61c7a3
Linda Vitali <linda.vitali2@studio.unibo.it>
2022-07-16 11:56:28 +0200

    feat: configuration parsing

--- src/main/scala/dev/atedeg/Configuration.scala
+++ src/main/scala/dev/atedeg/Configuration.scala
@@ -6,11 +6,19 @@ import better.files.File
 import io.circe.parser.{ parse => parseJsonString }
 import io.circe.{ Decoder, Json }
 import io.circe.yaml.parser
-import cats.implicits.*
+import cats.implicits._
+import Extensions._
 
-import Extensions.*
-
-sealed trait EntityType
+sealed trait EntityType {
+  override def toString: String = this match {
+    case Class => "class"
+    case Trait => "trait"
+    case Enum => "enum"
+    case Type => "type"
+    case Case => "case"
+    case Def => "def"
+  }
+}
 
 object EntityType {
 
@@ -32,6 +40,7 @@ case object Case extends EntityType
 case object Def extends EntityType
 
 final case class Entity(entityType: EntityType, link: String, name: String, packageName: String)
+final case class BaseEntity(entityType: EntityType, name: String)
 
 object Entity {
   private val entitiesFileName = "searchData.js"
@@ -54,49 +63,31 @@ object Entity {
   }
 }
 
-sealed trait IgnoredSelector
-final case class IgnoredClass(className: String) extends IgnoredSelector
-final case class IgnoredTrait(traitName: String) extends IgnoredSelector
-final case class IgnoredEnum(enumName: String) extends IgnoredSelector
-final case class IgnoredType(typeName: String) extends IgnoredSelector
-final case class IgnoredEnumCase(caseName: String) extends IgnoredSelector
-
-object IgnoredSelector {
-
-  def fromKind(name: String, kind: String): Option[IgnoredSelector] = kind match {
-    case "class" => Some(IgnoredClass(name))
-    case "trait" => Some(IgnoredTrait(name))
-    case "type" => Some(IgnoredType(name))
-    case "enum" => Some(IgnoredEnum(name))
-    case "case" => Some(IgnoredEnumCase(name))
-    case _ => None
-  }
-}
-
-sealed trait Selector {
-
-  def toIgnored: IgnoredSelector = this match {
-    case Class(className) => IgnoredClass(className)
-    case Trait(traitName) => IgnoredTrait(traitName)
-    case Enum(enumName) => IgnoredEnum(enumName)
-    case Type(typeName, _) => IgnoredType(typeName)
-    case EnumCase(caseName, _) => IgnoredEnumCase(caseName)
-  }
-}
-final case class Class(className: String) extends Selector
-final case class Trait(traitName: String) extends Selector
-final case class Enum(enumName: String) extends Selector
-final case class Type(typeName: String, lookupFile: String) extends Selector
-final case class EnumCase(caseName: String, lookupFile: String) extends Selector
-
-final case class Configuration(ignored: Set[IgnoredSelector], tables: List[TableConfig])
-
+final case class Configuration(ignored: Set[BaseEntity], tables: List[TableConfig])
 final case class TableConfig(
     name: String,
     termName: Option[String],
     definitionName: Option[String],
-    rows: List[Selector],
+    rows: List[BaseEntity],
 )
+
+object Configuration {
+  private val configFile = ".ubidoc.yaml"
+
+  def read(workingDir: File): Either[Error, Configuration] = Utils.parseFileWith(workingDir / configFile)(parse)
+
+  private[atedeg] def parse(raw: String): Either[Error, Configuration] =
+    parser.parse(raw).leftMap[Error](CirceParsingFailure).flatMap(parseJson)
+
+  private def parseJson(json: Json): Either[CirceDecodingFailure, Configuration] = {
+    import io.circe.generic.auto._
+    implicit val decodeEntity: Decoder[BaseEntity] = List(Class, Trait, Enum, Case, Type, Def)
+      .map(t => (t.toString, BaseEntity(t, _)))
+      .map(entry => Decoder.forProduct1(entry._1)(entry._2))
+      .reduceLeft(_ or _)
+    json.as[Configuration].leftMap(CirceDecodingFailure)
+  }
+}
 
 private object Utils {
 
@@ -105,59 +96,4 @@ private object Utils {
 
   def parseFileWith[A](file: File)(parser: String => Either[Error, A]): Either[Error, A] =
     openFile(file).flatMap(parser)
-}
-
-object AllEntities {
-  private val entitiesFileName = "searchData.js"
-
-  private def allEntitiesFile(workingDir: File): File =
-    workingDir / "target" / "site" / "scripts" / entitiesFileName
-
-  def read(workingDir: File): Either[Error, Set[IgnoredSelector]] =
-    Utils.parseFileWith(allEntitiesFile(workingDir))(parse)
-
-  private[atedeg] def parse(raw: String): Either[Error, Set[IgnoredSelector]] = {
-    val sanitizedRaw = raw.replaceFirst("pages = ", "").replaceFirst(";", "")
-    parseJsonString(sanitizedRaw).leftMap(CirceParsingFailure).flatMap(parseJson)
-  }
-
-  private def parseJson(json: Json): Either[Error, Set[IgnoredSelector]] = {
-    implicit val decodeIgnoredSelector: Decoder[Option[IgnoredSelector]] =
-      Decoder.forProduct2("n", "k")(IgnoredSelector.fromKind)
-    json.as[Set[Option[IgnoredSelector]]].map(_.dropNone).leftMap(CirceDecodingFailure)
-  }
-}
-
-object Configuration {
-  private val configFile = ".ubidoc.yaml"
-
-  def read(workingDir: File): Either[Error, Configuration] =
-    Utils.parseFileWith(workingDir / configFile)(parse)
-
-  private[atedeg] def parse(raw: String): Either[Error, Configuration] =
-    parser.parse(raw).leftMap[Error](CirceParsingFailure).flatMap(parseJson)
-
-  @SuppressWarnings(Array("org.wartremover.warts.IterableOps"))
-  private def parseJson(json: Json): Either[CirceDecodingFailure, Configuration] = {
-    import io.circe.generic.auto.*
-    implicit val decodeSelector: Decoder[Selector] = {
-      List[Decoder[Selector]](
-        Decoder[Class].widen,
-        Decoder[Trait].widen,
-        Decoder[Enum].widen,
-        Decoder[Type].widen,
-        Decoder[EnumCase].widen,
-      ).reduceLeft(_ or _)
-    }
-    implicit val decodeIgnoredSelector: Decoder[IgnoredSelector] = {
-      List[Decoder[IgnoredSelector]](
-        Decoder[IgnoredClass].widen,
-        Decoder[IgnoredTrait].widen,
-        Decoder[IgnoredEnum].widen,
-        Decoder[IgnoredType].widen,
-        Decoder[IgnoredEnumCase].widen,
-      ).reduceLeft(_ or _)
-    }
-    json.as[Configuration].leftMap(CirceDecodingFailure)
-  }
 }
