43c5a4ab153d2e02ef36429c92d71fde564c1588
Linda Vitali <linda.vitali2@studio.unibo.it>
2022-07-16 18:20:57 +0200

    fix: link generation

--- src/main/scala/dev/atedeg/HtmlParsing.scala
+++ src/main/scala/dev/atedeg/HtmlParsing.scala
@@ -21,7 +21,7 @@ object HtmlParsing {
       entity: Entity,
       allEntities: Set[Entity],
   ): Either[Error, String] = {
-    val searchQuery = s"#${entity.entityId.map(_ + " > ").getOrElse("")}div.cover > div.doc"
+    val searchQuery = s"${entity.entityId.map("div#" + _ + " ").getOrElse("")}div.cover > div.doc"
     extractTagFromDocument(file, document, searchQuery, allEntities)
   }
 
@@ -35,10 +35,10 @@ object HtmlParsing {
 
   private def toMarkdown(es: Iterable[Node], allEntities: Set[Entity]): Either[Error, String] = {
     def isLink(e: Element): Boolean = e.tagName == "a"
-    def toMarkdownLink(e: Element) = lookupLinkFor(extractName(e)).map(l => s"[$l](${e.text})")
-    def extractName(e: Element): String = e.attr("href").replace(".html", "")
+    def toMarkdownLink(e: Element) = lookupLinkFor(extractName(e)).map(l => s"[${e.text}]($l)")
+    def extractName(e: Element): String = e.attr("href").replace(".html", "").split('$').last.split("/").last
     def lookupLinkFor(name: String): Either[Error, String] =
-      allEntities.find(_.name == name).map(_.link.replace("/", ".")).toRight(MissingLink(name))
+      allEntities.find(_.name == name).map("../" + _.link).toRight(MissingLink(name))
 
     es.foldLeft("".asRight[Error]) { (acc, elem) =>
       elem match {
