d2a127cac99e2856821d2b701e230bb3c02d6e73
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2022-07-17 11:16:19 +0200

    refactor: reduce ignored warts

--- src/main/scala/dev/atedeg/TableUtils.scala
+++ src/main/scala/dev/atedeg/TableUtils.scala
@@ -1,14 +1,13 @@
 package dev.atedeg
 
 import dev.atedeg.EntityConversion.entityToRow
-
 import better.files.File
-import cats.syntax.all._
+import Extensions._
 
 object TableUtils {
 
   def entitiesToRows(table: Table[Entity], baseDir: File, allEntities: Set[Entity]): Either[Error, Table[Row]] =
     table.rows
-      .traverse(entityToRow(_, baseDir, allEntities))
+      .traverseError(entityToRow(_, baseDir, allEntities))
       .map(Table(table.title, table.termName, table.definitionName, _))
 }
