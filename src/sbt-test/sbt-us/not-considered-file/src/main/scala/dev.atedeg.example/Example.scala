a55098e13e8634c0113c2a00b7a6b0fad6418fde
Linda Vitali <linda.vitali2@studio.unibo.it>
2022-07-17 17:27:13 +0200

    test: add not considered file test

---
+++ src/sbt-test/sbt-us/not-considered-file/src/main/scala/dev.atedeg.example/Example.scala
@@ -0,0 +1,15 @@
+package dev.atedeg.example
+/**
+ * Example doc.
+ */
+case class Example()
+
+/**
+ * Example2 doc.
+ */
+case class Example2()
+
+/**
+ * Example3 doc.
+ */
+case class Example3()
