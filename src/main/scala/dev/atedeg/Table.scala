26426df1b59829723d364b6f5b4e9f86e7c22a2d
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2022-07-17 12:01:50 +0200

    fix: table serialization only applies to Tables with Rows and not entitites

--- src/main/scala/dev/atedeg/Table.scala
+++ src/main/scala/dev/atedeg/Table.scala
@@ -2,28 +2,7 @@ package dev.atedeg
 
 import java.util.Locale.US
 
-import scala.util.Try
-
-import better.files.File
-import net.steppschuh.markdowngenerator.table.Table.ALIGN_LEFT
-import net.steppschuh.markdowngenerator.table.Table.Builder
-
-final case class Table[E](title: String, termName: String, definitionName: String, rows: List[E]) {
-
-  def map[A](f: E => A): Table[A] = Table(title, termName, definitionName, rows.map(f))
-
-  override def toString: String = {
-    val builder = new Builder().withAlignment(ALIGN_LEFT).addRow(termName, definitionName)
-    for { Row(term, definition) <- rows } builder.addRow(term, definition)
-    builder.build.serialize
-  }
-
-  def serialize(targetDir: File): Try[Unit] = {
-    val file = targetDir / s"$title.md"
-    Try(file.createFileIfNotExists(createParents = true).write(this.toString))
-  }
-}
-
+final case class Table[E](title: String, termName: String, definitionName: String, rows: List[E])
 final case class Row private (term: String, definition: String)
 
 object Row {
