25b3d5012c649acee6da02060e57b87591f321d1
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2022-07-29 18:12:58 +0200

    test: add test to check correct enum case handling

---
+++ src/sbt-test/sbt-us/overlapping-enum-cases/src/main/scala/dev.atedeg.example/Example.scala
@@ -0,0 +1,22 @@
+package dev.atedeg.example
+
+/**
+ * Example doc.
+ */
+enum Enum1:
+
+  /**
+   * Enum1.Case1
+   */
+  case Case1
+
+
+/**
+ * Example2 doc.
+ */
+enum Enum2:
+
+  /**
+   * Enum2.Case1
+   */
+  case Case1
