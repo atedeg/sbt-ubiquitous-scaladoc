ed4b4f981b33382ee3ec78f2eaa52f7e2fe99272
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2022-08-19 16:04:26 +0200

    fix: only normalize names that are not explicitly provided

--- src/main/scala/dev/atedeg/Table.scala
+++ src/main/scala/dev/atedeg/Table.scala
@@ -3,12 +3,4 @@ package dev.atedeg
 import java.util.Locale.US
 
 final case class Table[E](title: String, termName: String, definitionName: String, rows: List[E])
-final case class Row private (term: String, definition: String)
-
-object Row {
-
-  private def normalizeName(name: String): String =
-    name.split("(?=\\p{Upper})").toList.map(_.toLowerCase(US)).mkString(" ").capitalize
-
-  def from(term: String, definition: String): Row = Row(normalizeName(term), definition)
-}
+final case class Row(term: String, definition: String)
